17cbc99a61858de260e1426a07413501d26d1219
angerls.page <realrealfeelings@outlook.com>
2021-06-21 02:46:16 +0300

    typo fix

--- auth.component/auth.component.ts
+++ auth.component/auth.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-auth',
-  template: 'auth.component.html',
+  templateUrl: 'auth.component.html',
 })
 export class AuthComponent {
   title: 'authorization';
